a8b816cee669876cf26fe617453a4b7eeb682a01
muthu050700 <muthumuthu7699@gmail.com>
2024-06-22 07:58:00 +0530

    checking

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,7 +3,7 @@ import { Fade } from "react-awesome-reveal";
 const Home = () => {
   return (
     <>
-      <div className=" bg-white text-black py-40 container mx-auto flex flex-col justify-center gap-10 lg:flex-row md:items-center ">
+      <div className=" bg-white text-black py-40 md:py-[230px] container mx-auto flex flex-col justify-center gap-10 lg:flex-row md:items-center ">
         {/* Left side content */}
         <div className="flex flex-col ml-2 gap-2 lg:flex-1 lg:gap-5  lg:pl-20">
           <p className="font-bold text-2xl md:text-3xl lg:text-4xl">
