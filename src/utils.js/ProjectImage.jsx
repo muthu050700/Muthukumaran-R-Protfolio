80196649041abcf44ae38d9163fa33fff443f552
muthu050700 <muthumuthu7699@gmail.com>
2024-06-30 02:28:11 +0530

    checking

--- src/utils.js/ProjectImage.jsx
+++ src/utils.js/ProjectImage.jsx
@@ -2,7 +2,7 @@ const dataForProject = [
   {
     name: "Todo App",
     used: ["React js", "Vite", "Tailwind CSS"],
-    image: "./assets/project-1.jpeg",
+    image: "./assets/profile-img.jpeg",
     demo: "https://symphonious-crepe-1a9a89.netlify.app/",
     source: "https://github.com/muthu050700/React-Project-3",
   },
