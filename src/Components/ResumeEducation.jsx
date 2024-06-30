7062a4f59ae5794d870534a014c2db95a3d2e35b
muthu050700 <muthumuthu7699@gmail.com>
2024-06-30 22:43:18 +0530

    added some new content

--- src/Components/ResumeEducation.jsx
+++ src/Components/ResumeEducation.jsx
@@ -9,26 +9,29 @@ const ResumeEducation = () => {
     >
       <h1 className="text-3xl font-bold ">Education</h1>
       <p className="text-justify text-lg leading-7 px-2 md:px-0">
-        I am currently enhancing my front-end development skills through a
-        6-month course and hold a degree in Computer Science Engineering.
+        I hold a degree in Computer Science Engineering and am currently
+        enhancing my front-end development skills through a 6-month course. This
+        course focuses on advanced techniques and modern web development tools.
+        My goal is to stay updated with industry trends and improve my
+        proficiency in creating user-friendly web applications.
       </p>
 
       <div className=" grid lg:grid-cols-1 xl:grid-cols-2 gap-5 h-[400px] md:h-[200px]  overflow-y-auto lg:hover:overflow-y-auto px-3 lg:overflow-y-hidden scrolling">
-        <div className="flex flex-col justify-center items-center shadow-md shadow-black  bg-neutral-900 text-[#C73659] py-4 px-3 my-2 w-[full]  h-[170px] gap-3">
+        <div className="flex flex-col justify-center items-center shadow-md shadow-black  bg-neutral-900 text-[#C73659] py-4 px-3 my-2 w-[full]  h-[170px] gap-3 cursor-pointer">
           <p className="font-medium text-sm">2023 - 2024 (6 Months)</p>
           <p className="font-bold text-xl text-center">Front-End Developer</p>
           <p className="font-medium text-sm text-center">GUVI</p>
         </div>
-        <div className="flex flex-col justify-center items-center shadow-md shadow-black  bg-neutral-900 text-[#C73659] py-4 px-3 my-2 w-[full] h-[170px]  gap-3">
+        <div className="flex flex-col justify-center items-center shadow-md shadow-black  bg-neutral-900 text-[#C73659] py-4 px-3 my-2 w-[full] h-[170px]  gap-3 cursor-pointer">
           <p className="font-medium text-sm">2018 - 2022</p>
           <p className="font-bold text-xl text-center">
-            Computer Science Engineering
+            Computer Science and Engineering
           </p>
           <p className="font-medium text-sm text-center">
             Rajalakshmi Institute Of Technology.
           </p>
         </div>
-        <div className="flex flex-col justify-center items-center shadow-md shadow-black  bg-neutral-900 text-[#C73659] py-4 px-3 my-2 w-[full] h-[170px]  gap-3">
+        <div className="flex flex-col justify-center items-center shadow-md shadow-black  bg-neutral-900 text-[#C73659] py-4 px-3 my-2 w-[full] h-[170px]  gap-3 cursor-pointer">
           <p className="font-medium text-sm">2017 - 2018</p>
           <p className="font-bold text-xl text-center">Computer Science</p>
           <p className="font-medium text-sm text-center">
