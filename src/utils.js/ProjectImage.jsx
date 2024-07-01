4f8252582bbe1a4d6384da951b5d13936989fd21
muthu050700 <muthumuthu7699@gmail.com>
2024-07-01 06:43:39 +0530

    added new project

--- src/utils.js/ProjectImage.jsx
+++ src/utils.js/ProjectImage.jsx
@@ -14,25 +14,39 @@ const dataForProject = [
     source: "https://github.com/muthu050700/React-Guvi-Task-2",
   },
   {
+    name: "Shopify",
+    used: ["React js", "Vite", "Bootstrap"],
+    image: "/project-3.png",
+    demo: "https://magical-sopapillas-d89799.netlify.app/",
+    source: "https://github.com/muthu050700/React-Guvi-Task-1",
+  },
+  {
     name: "Student Calculator App",
     used: ["HTML", "CSS", "Javascript"],
-    image: "/project-3.png",
+    image: "/project-4.png",
     demo: "https://effulgent-lokum-9f0737.netlify.app/",
     source: "https://github.com/muthu050700/Day8-Task-DOM-Calculator-",
   },
   {
     name: "Newsletter-sign-up",
     used: ["HTML", "CSS", "Javascript"],
-    image: "/project-4.png",
+    image: "/project-5.png",
     demo: "https://sensational-cranachan-a368e1.netlify.app/",
     source: "https://sensational-cranachan-a368e1.netlify.app",
   },
   {
     name: "FAQ-Accordion",
     used: ["HTML", "CSS", "Javascript"],
-    image: "/project-5.png",
+    image: "/project-6.png",
     demo: "https://prismatic-moonbeam-149d7c.netlify.app/",
     source: "https://prismatic-moonbeam-149d7c.netlify.app",
+  },
+  {
+    name: "Recipe Page",
+    used: ["HTML", "CSS"],
+    image: "/project-7.png",
+    demo: "https://stalwart-tartufo-d6c6c1.netlify.app/",
+    source: "https://github.com/muthu050700/Frontend-Mentor---Task-1-HTML-CSS-",
   },
 ];
 
